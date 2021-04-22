b4894e3b90413976f1948bda3613c190fd32a57b
JX Wang <jxwang92@gmail.com>
2021-04-23 01:27:01 +0900

    Tired and sleepy. I forgot what change I made in this commit

--- src/meta_data.rs
+++ src/meta_data.rs
@@ -4,4 +4,4 @@ use once_cell::sync::Lazy;
 
 pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
-pub static NUM_CPUS: Lazy<usize> = Lazy::new(||num_cpus::get());
+pub static NUM_CPUS: Lazy<usize> = Lazy::new(num_cpus::get);
