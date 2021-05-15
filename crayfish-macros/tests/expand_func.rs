3a5a487aa04499d05ae1228c2e88a7dc0148ca18
JX Wang <jxwang92@gmail.com>
2021-05-15 19:55:59 +0900

    add attrs

--- crayfish-macros/tests/expand_func.rs
+++ crayfish-macros/tests/expand_func.rs
@@ -1,4 +1,3 @@
-use crayfish::runtime::ApgasContext;
 use crayfish_macros::*;
 
 use futures::FutureExt;
@@ -6,14 +5,23 @@ use futures::future::BoxFuture;
 
 #[activity]
 #[allow(unused_variables)]
-async fn foo(ctx: &mut impl ApgasContext, a: i32, b: i64, c: Vec<usize>, d: String) -> i32 {
+async fn foo(a: i32, b: i64, c: Vec<usize>, d: String) -> i32 {
     123
 }
 
-#[activity]
-#[allow(unused_variables)]
-fn bar(ctx: &mut impl ApgasContext, a: i32, b: i64, c: Vec<usize>, d: String) -> BoxFuture<'static, usize>{
-    async move{
+mod inside{
+    extern crate crayfish as duang;
+    #[crayfish_macros::activity(crate="duang")]
+    #[allow(unused_variables)]
+    async fn foo(a: i32, b: i64, c: Vec<usize>, d: String) -> i32 {
         123
-    }.boxed()
+    }
 }
+//
+// #[activity]
+// #[allow(unused_variables)]
+// fn bar(ctx: &mut impl ApgasContext, a: i32, b: i64, c: Vec<usize>, d: String) -> BoxFuture<'static, usize>{
+//     async move{
+//         123
+//     }.boxed()
+// }
