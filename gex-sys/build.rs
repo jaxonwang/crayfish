5cbd4c662ed6b7fc107043ff0289977222372094
JX Wang <jxwang92@gmail.com>
2021-05-24 18:36:59 +0900

    refactor build context

--- gex-sys/build.rs
+++ gex-sys/build.rs
@@ -20,26 +20,26 @@ const GASNET_CONDUIT_LIST: [&str; 6] = ["ibv", "mpi", "udp", "smp", "ucx", "arie
 const GASNET_WRAPPER: &str = "gasnet_wrapper";
 const GASNET_LIBAMUDP: &str = "amudp";
 const REBUILD_IF_ENVS_CHANGE: [&str; 17] = [
-// mpi flags
+    // mpi flags
     "MPI_CC",
     "MPI_CFLAGS",
     "MPI_LIBS",
-// ibv flags
+    // ibv flags
     "IBV_HOME",
     "IBV_CFLAGS",
     "IBV_LIBS",
     "IBV_LDFLAGS",
     "FH_CFLAGS",
-// common C/CXX flags
-    "CC",      
-    "CFLAGS",  
-    "LDFLAGS", 
-    "LIBS",    
+    // common C/CXX flags
+    "CC",
+    "CFLAGS",
+    "LDFLAGS",
+    "LIBS",
     "CPPFLAGS",
-    "CPP",     
-    "CXX",     
+    "CPP",
+    "CXX",
     "CXXFLAGS",
-    "CXXCPP",  
+    "CXXCPP",
 ];
 
 #[allow(dead_code)]
@@ -86,6 +86,52 @@ fn mpi_conduit_flags() {
 
 fn ibv_conduit_flags() {}
 
+mod mpi_probe {
+    use super::*;
+    fn mpicc_path_from_gasnet(ctx: BuildContext) {
+        // let conduit_header_dir = conduit_header_dir
+    }
+}
+
+struct BuildContext {
+    out_dir: PathBuf,
+    working_dir: PathBuf,         // buildpath/out/gasnet
+    gasnet_src_dir: PathBuf,      // .src/gasnet
+    gasnet_lib_dir: PathBuf,      // buildpath/out/lib
+    gasnet_include_dir: PathBuf,  // buildpath/out/include
+    conduit_include_dir: PathBuf, // buildpath/out/include/udp-conduit
+    conduit: GasnetConduit,
+}
+
+impl BuildContext {
+    fn new(conduit: GasnetConduit) -> Self {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let out_dir = fs::canonicalize(&out_dir).unwrap();
+        let gasnet_lib_dir = out_dir.join("lib");
+        let gasnet_include_dir = out_dir.join("include");
+        let conduit_include_dir = gasnet_include_dir.join(format!("{}-conduit", conduit.as_ref()));
+        let gasnet_src_dir = fs::canonicalize("gasnet").unwrap();
+        let working_dir = out_dir.join("gasnet");
+        // create working directory
+        if !working_dir.exists() {
+            fs::create_dir(&working_dir).unwrap();
+        }
+        BuildContext {
+            out_dir,
+            working_dir,
+            gasnet_lib_dir,
+            gasnet_include_dir,
+            gasnet_src_dir,
+            conduit_include_dir,
+            conduit,
+        }
+    }
+
+    fn build_script_path(&self, script_name: impl AsRef<Path>) -> PathBuf {
+        self.gasnet_src_dir.join(script_name)
+    }
+}
+
 pub fn main() {
     // TODO: cleanup everythin before install
 
@@ -99,32 +145,23 @@ pub fn main() {
     conduit_enabled.sort(); // order by precedence
 
     let conduit_to_use = conduit_enabled[0];
+    let ctx = BuildContext::new(conduit_to_use);
 
     // config contains version info
     println!("cargo:rerun-if-changed=src/gasnet_wrapper.h");
     println!("cargo:rerun-if-changed=src/gasnet_wrapper.c");
     println!("cargo:rerun-if-changed=gasnet");
-	for env in REBUILD_IF_ENVS_CHANGE.iter(){
-		println!("cargo:rerun-if-env-changed={}", env);
-	}
-	
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let out_dir = Path::new(&out_dir);
-    let libdir = out_dir.join("lib");
-    let gasnet_dir = fs::canonicalize("gasnet").unwrap();
-    let abs_path = |s: &str| gasnet_dir.join(s);
-    let working_dir = out_dir.join("gasnet");
-    if !working_dir.exists() {
-        fs::create_dir(&working_dir).unwrap();
+    for env in REBUILD_IF_ENVS_CHANGE.iter() {
+        println!("cargo:rerun-if-env-changed={}", env);
     }
 
     // bootstrap
     let mut bt = Command::new("/bin/sh");
-    bt.arg(abs_path("Bootstrap")).current_dir(&working_dir);
-    if !abs_path("configure").exists() {
+    bt.arg(ctx.build_script_path("Bootstrap"))
+        .current_dir(&ctx.working_dir);
+    if !ctx.build_script_path("configure").exists() {
         // no re-bootstrap
-        if !abs_path("Bootstrap").exists() {
+        if !ctx.build_script_path("Bootstrap").exists() {
             panic!("Can not find the Bootstrap script for GASNET. Did your forget \"git clone --recursive\"?")
         }
         run_command("bootstrap", &mut bt);
@@ -134,8 +171,8 @@ pub fn main() {
     let mut cfg = Command::new("/bin/sh");
     let envs = vec![("CFLAGS", "-fPIC"), ("CXXFLAGS", "-fPIC")];
     cfg.envs(envs);
-    cfg.arg(abs_path("configure"))
-        .arg(format!("--prefix={}", out_dir.display()))
+    cfg.arg(ctx.build_script_path("configure"))
+        .arg(format!("--prefix={}", &ctx.out_dir.display()))
         .arg("--enable-seq")
         .arg("--disable-parsync")
         .arg("--disable-par");
@@ -155,19 +192,19 @@ pub fn main() {
             }
         }
     }
-    cfg.current_dir(&working_dir);
+    cfg.current_dir(&ctx.working_dir);
     run_command("configure", &mut cfg);
 
     // make
     let mut mk = Command::new("make");
     mk.arg(GASNET_THREADING_ENV).arg("-j");
-    mk.current_dir(&working_dir);
+    mk.current_dir(&ctx.working_dir);
     run_command("make", &mut mk);
 
     // install
     let mut is = Command::new("make");
     is.arg("install");
-    is.current_dir(&working_dir);
+    is.current_dir(&ctx.working_dir);
     run_command("install", &mut is);
 
     // common flags
@@ -178,7 +215,10 @@ pub fn main() {
     );
     println!("cargo:rustc-link-lib=static={}", libgasnet);
     println!("cargo:rustc-link-lib=dylib=m"); // specified by udp-conduit/conduit.mak
-    println!("cargo:rustc-link-search=native={}", libdir.display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        ctx.gasnet_lib_dir.display()
+    );
 
     // conduit flags
     match conduit_to_use {
@@ -187,15 +227,12 @@ pub fn main() {
         GasnetConduit::Ibv => ibv_conduit_flags(),
     }
 
-    let conduit_dir = format!("include/{}-conduit", conduit_to_use.as_ref());
-    let headers: Vec<&str> = vec![&conduit_dir, "include"];
-    let headers: Vec<PathBuf> = headers.iter().map(|dir| out_dir.join(dir)).collect();
-
     // compile wrapper
     let mut builder = cc::Build::new();
     builder
         .file(format!("src/{}.c", GASNET_WRAPPER))
         .include("src");
+    let headers = [&ctx.gasnet_include_dir, &ctx.conduit_include_dir];
     for h in headers.iter() {
         builder.include(h);
     }
@@ -203,7 +240,7 @@ pub fn main() {
 
     // ask rustc to link against the wrapper
     println!("cargo:rustc-link-lib=static={}", GASNET_WRAPPER);
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    println!("cargo:rustc-link-search=native={}", ctx.out_dir.display());
 
     // bindgen
     let mut clang_args = vec![];
@@ -220,7 +257,9 @@ pub fn main() {
         .allowlist_var("GASNET_.*")
         .generate()
         .expect("Unable to generate bindings");
-    bindings.write_to_file(out_dir.join("bindings.rs")).unwrap();
+    bindings
+        .write_to_file(ctx.out_dir.join("bindings.rs"))
+        .unwrap();
 }
 
 #[cfg(test)]
