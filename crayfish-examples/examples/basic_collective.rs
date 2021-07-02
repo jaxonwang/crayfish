8fef0ded4d90feb40beadc254025f09645d10011
JX Wang <jxwang92@gmail.com>
2021-07-02 18:57:18 +0900

    fix all gather bugs

--- crayfish-examples/examples/basic_collective.rs
+++ crayfish-examples/examples/basic_collective.rs
@@ -32,7 +32,24 @@ async fn main() {
     collective::broadcast(root, &mut m).await;
     assert_eq!(m, map_to_broadcast);
 
-    let value = global_id::here() as usize;
-    let values = collective::all_gather(value).await;
+    let value = global_id::here().to_string();
+    let values = collective::all_gather(value.clone()).await;
+    let expected: Vec<String> = (0..global_id::world_size())
+        .map(|i| i.to_string())
+        .collect();
+    assert_eq!(values, expected);
     info!("all gather ranks {:?}", values);
+
+    // many all gather
+    let mut all_gather_futures = vec![];
+    for _ in 0..10 {
+        let value = (global_id::here() + 1).to_string();
+        all_gather_futures.push(collective::all_gather(value.clone()));
+    }
+    for f in all_gather_futures {
+        let expected: Vec<String> = (0..global_id::world_size())
+            .map(|i| (i + 1).to_string())
+            .collect();
+        assert_eq!(f.await, expected);
+    }
 }
